170c0457ea4a5ded792e2817702cb2c7ca480701
ifarrag <ifarrrag@gmail.com>
2024-06-17 16:08:24 -0700

    UNIX command line interpreter

--- siri.c
+++ siri.c
@@ -43,7 +43,7 @@ int main(int ac, char **av __attribute__((unused)), char **env)
 				if (pid == 0)
 				{
 					execve(arrs[0], arrs, env);
-					write(1 , arrs[0], 4);
+					write(1, arrs[0], 4);
 				}
 				else
 				{
